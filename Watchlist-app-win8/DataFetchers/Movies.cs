18a9babc43bbc4a3d5d64dc184fea9de564e0821
Kostya Sergeev <kostyasergeev93@gmail.com>
2014-03-06 09:43:43 +0400

    add movie info class

--- Watchlist-app-win8/DataFetchers/Movies.cs
+++ Watchlist-app-win8/DataFetchers/Movies.cs
@@ -1,1 +1,35 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Watchlist_app_win8.DataFetchers
+{
+
+    //Movie metadata
+    //
+    //
+    //
+
+    class Movie
+    {
+        public Movie()
+        {
+            id = "empty";
+        }
+        public string Title { get; set; }
+        public string overview { get; set; }
+        public string poster_path { get; set; }
+        public int Watch_flag { get; set; }
+        public string Release_Date { get; set; }
+        public string source { get; set; }
+        public string budget { get; set; }
+        public string runtime { get; set; }
+        public string Vote_Average { get; set; }
+        public string revenue { get; set; }
+        public string id { get; set; }
+
+        //public ObservableCollection<Movie> movieCall = new ObservableCollection<Movie>();
+    }
+}
